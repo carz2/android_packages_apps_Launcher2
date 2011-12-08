5553c1893825e2717723530db87972b4979148d1
nebkat <nebkat@gmail.com>
2011-12-08 17:13:35 +0000

    Preferences: Fade Page Indicator (Workspace)
    Change-Id: I7c30adaac1ddaea69e409410b53185926a9266f6

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -2403,7 +2403,6 @@ public final class Launcher extends Activity
                         ((LauncherTransitionable) fromView).onLauncherTransitionEnd(instance,
                                 alphaAnim, true);
                     }
-                    mWorkspace.hideScrollingIndicator(false);
                 }
             });
 
